8dc62f087d0c27f853d59a4aaccb6674cf2ea7c6
Charles Nicholson <charles.nicholson@gmail.com>
2022-02-20 13:12:09 -0500

    4514 msvc

--- paland.cc
+++ paland.cc
@@ -41,7 +41,6 @@
 #include "../../nanoprintf.h"
 
 #if NANOPRINTF_HAVE_GCC_WARNING_PRAGMAS
-  #pragma GCC diagnostic push
   #if NANOPRINTF_CLANG
     #pragma GCC diagnostic ignored "-Wc++98-compat-pedantic"
     #ifndef __APPLE__
@@ -50,10 +49,13 @@
   #endif
 #endif
 
+#ifdef _MSC_VER
+  #pragma warning(disable:4514) // unreferenced inline function removed
+#endif
+
 #include "../doctest.h"
 
 #if NANOPRINTF_HAVE_GCC_WARNING_PRAGMAS
-  #pragma GCC diagnostic push
   #if NANOPRINTF_CLANG
     #pragma GCC diagnostic ignored "-Wformat-pedantic"
   #endif
